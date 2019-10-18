79db36c406f63aa4469217c55e206657fb282b50
Kristin Barr <kristinbarr@gmail.com>
2019-10-18 12:02:01 -0400

    implemented register and login routes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,42 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const generateToken = require('./token-middleware')
+const Users = require('../users/users-model')
 
 router.post('/register', (req, res) => {
-  // implement registration
-});
+  let user = req.body // get user sent in request
+  const hash = bcrypt.hashSync(user.password, 10) // hash the password
+  user.password = hash // set hased password as the password
+
+  // add the new user to database
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved) // send back the saved user
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'cannot add the user', err })
+    })
+})
 
 router.post('/login', (req, res) => {
-  // implement login
-});
+  let { username, password } = req.body // capture username, pass from request
 
-module.exports = router;
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+})
+
+module.exports = router
